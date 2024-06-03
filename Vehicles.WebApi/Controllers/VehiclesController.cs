50e3b8504ed93d64325f09130b3f39bd67a8beb7
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-06-03 13:59:22 +0200

    add controller get, getall and delete endpoints

--- Vehicles.WebApi/Controllers/VehiclesController.cs
+++ Vehicles.WebApi/Controllers/VehiclesController.cs
@@ -13,13 +13,106 @@ public class VehiclesController : ControllerBase
         _configuration = configuration;
     }
 
+    [HttpGet]
+    public ActionResult Get()
+    {
+        try
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            var connectionString = _configuration.GetConnectionString("Default");
+            using var connection = new NpgsqlConnection(connectionString);
+
+            var commandText = "SELECT * FROM \"Vehicle\"";
+
+            using var command = new NpgsqlCommand(commandText, connection);
+
+            connection.Open();
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                while (reader.Read())
+                {
+                    vehicles.Add(new Vehicle
+                    {
+                        Id = Guid.Parse(reader[0].ToString()),
+                        MakeId = Guid.TryParse(reader[1].ToString(), out var result) ? result : null,
+                        Model = reader[2].ToString(),
+                        Color = reader[3].ToString(),
+                        Year = DateTime.Parse(reader[4].ToString()),
+                        ForSale = bool.Parse(reader[5].ToString())
+                    });
+                }
+            }
+
+            if (vehicles.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(vehicles);
+
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult Get(Guid id)
+    {
+        try
+        {
+            Vehicle vehicle = new Vehicle();
+            var connectionString = _configuration.GetConnectionString("Default");
+            using var connection = new NpgsqlConnection(connectionString);
+
+            var commandText = "SELECT * FROM \"Vehicle\" WHERE \"Id\" = @Id";
+
+            using var command = new NpgsqlCommand(commandText, connection);
+
+            command.Parameters.AddWithValue("@Id", NpgsqlTypes.NpgsqlDbType.Uuid, id);
+
+            connection.Open();
+
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    vehicle.Id = Guid.Parse(reader[0].ToString());
+                    vehicle.MakeId = Guid.TryParse(reader[1].ToString(), out var result) ? result : null;
+                    vehicle.Model = reader[2].ToString();
+                    vehicle.Color = reader[3].ToString();
+                    vehicle.Year = DateTime.Parse(reader[4].ToString());
+                    vehicle.ForSale = bool.Parse(reader[5].ToString());
+                }
+            }
+
+            connection.Close();
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vehicle);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
     [HttpPost]
     public ActionResult Insert(Vehicle vehicle)
     {
         try
         {
             var connectionString = _configuration.GetConnectionString("Default");
-
             using var connection = new NpgsqlConnection(connectionString);
 
             var commandText = "INSERT INTO \"Vehicle\" (\"Id\", \"MakeId\", \"Model\", \"Color\", \"Year\", \"ForSale\") VALUES (@Id, @MakeId, @Model, @Color, @Year, @ForSale)";
@@ -27,7 +120,7 @@ public class VehiclesController : ControllerBase
             using var command = new NpgsqlCommand(commandText, connection);
 
             command.Parameters.AddWithValue("@Id", NpgsqlTypes.NpgsqlDbType.Uuid, Guid.NewGuid());
-            command.Parameters.AddWithValue("@MakeId", NpgsqlTypes.NpgsqlDbType.Uuid, vehicle.MakeId);
+            command.Parameters.AddWithValue("@MakeId", NpgsqlTypes.NpgsqlDbType.Uuid, vehicle.MakeId is null ? DBNull.Value : vehicle.MakeId);
             command.Parameters.AddWithValue("@Model", vehicle.Model.ToString());
             command.Parameters.AddWithValue("@Color", vehicle.Color.ToString());
             command.Parameters.AddWithValue("@Year", NpgsqlTypes.NpgsqlDbType.TimestampTz, vehicle.Year);
@@ -45,6 +138,40 @@ public class VehiclesController : ControllerBase
             }
 
             return Ok("Successfully added");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(Guid id)
+    {
+        try
+        {
+            var connectionString = _configuration.GetConnectionString("Default");
+            using var connection = new NpgsqlConnection(connectionString);
+
+            var commandText = "DELETE FROM \"Vehicle\" WHERE \"Id\" = @Id";
+
+            using var command = new NpgsqlCommand(commandText, connection);
+
+            command.Parameters.AddWithValue("@Id", NpgsqlTypes.NpgsqlDbType.Uuid, id);
+
+            connection.Open();
+
+            var commits = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            if (commits == -1)
+            {
+                return BadRequest();
+            }
+
+            return Ok("Successfully deleted");
+
         }
         catch (Exception ex)
         {
--- Vehicles.WebApi/Models/Vehicle.cs
+++ Vehicles.WebApi/Models/Vehicle.cs
@@ -5,7 +5,7 @@ namespace Vehicles.WebApi.Models;
 public class Vehicle
 {
     public Guid Id { get; set; }
-    public Guid MakeId { get; set; }
+    public Guid? MakeId { get; set; }
     public string? Model { get; set; }
     [Required]
     public string? Color { get; set; }
