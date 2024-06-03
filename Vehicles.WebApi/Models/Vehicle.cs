1fe1f66ba017a242b3a7be75d00865abe5e92730
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-06-03 10:28:44 +0200

    add vehicle model

---
+++ Vehicles.WebApi/Models/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.WebApi.Models;
+
+public class Vehicle
+{
+    public Guid Id { get; set; }
+    public string? Model { get; set; }
+    [Required]
+    public string? Colour { get; set; }
+    public DateTime Year { get; set; }
+    public Guid MakeId { get; set; }
+    public Make Make { get; set; }
+}
