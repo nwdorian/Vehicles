ee1ae14185afb4a36d06a41dffd1ba09d5609ddd
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-06-03 10:17:50 +0200

    add create tables sql

---
+++ SQL/Vehicles_Create_Tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS "Make"(
+	"Id" UUID DEFAULT gen_random_uuid(),
+	"Name" VARCHAR(100) NOT NULL,
+	CONSTRAINT "PK_Make" PRIMARY KEY ("Id")
+);
+
+CREATE TABLE IF NOT EXISTS "Vehicle"(
+	"Id" UUID DEFAULT gen_random_uuid(),
+	"MakeId" UUID,
+	"Model" VARCHAR(100),
+	"Colour" VARCHAR(40) NOT NULL,
+	"Year" TIMESTAMPTZ,
+	"ForSale" BOOLEAN NOT NULL,
+	CONSTRAINT "PK_Vehicle" PRIMARY KEY ("Id"),
+	CONSTRAINT "FK_Vehicle_Make_MakeId" FOREIGN KEY ("MakeId") REFERENCES "Make"("Id")
+);
