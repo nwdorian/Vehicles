e207ed554f5563b2536dc7da6a459f914cafd7a3
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-06-03 10:37:09 +0200

    add ForSale property and rename colour to color

--- Vehicles.WebApi/Models/Vehicle.cs
+++ Vehicles.WebApi/Models/Vehicle.cs
@@ -7,8 +7,10 @@ public class Vehicle
     public Guid Id { get; set; }
     public string? Model { get; set; }
     [Required]
-    public string? Colour { get; set; }
+    public string? Color { get; set; }
     public DateTime Year { get; set; }
+    [Required]
+    public bool ForSale { get; set; }
     public Guid MakeId { get; set; }
     public Make? Make { get; set; }
 }
