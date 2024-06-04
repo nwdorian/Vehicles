cecc96a8fcaccc02799053e6fef2c93cd5cf9ead
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-06-04 11:16:26 +0200

    refactor: add Insert, Update and Delete methods to VehicleService

--- Vehicles.Service/VehicleService.cs
+++ Vehicles.Service/VehicleService.cs
@@ -17,4 +17,25 @@ public class VehicleService
 
         return await vehicleRepository.GetAsync(id);
     }
+
+    public async Task<bool> InsertAsync(Vehicle vehicle)
+    {
+        VehicleRepository vehicleRepository = new VehicleRepository();
+
+        return await vehicleRepository.InsertAsync(vehicle);
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        VehicleRepository vehicleRepository = new VehicleRepository();
+
+        return await vehicleRepository.DeleteAsync(id);
+    }
+
+    public async Task<bool> UpdateAsync(Guid id, Vehicle vehicle)
+    {
+        VehicleRepository vehicleRepository = new VehicleRepository();
+
+        return await vehicleRepository.UpdateAsync(id, vehicle);
+    }
 }
