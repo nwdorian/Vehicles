283854dac0d717e4765b153606423d5c92236ddb
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-06-04 11:13:21 +0200

    refactor: add Get method to VehicleService

--- Vehicles.Service/VehicleService.cs
+++ Vehicles.Service/VehicleService.cs
@@ -10,4 +10,11 @@ public class VehicleService
 
         return await vehicleRepository.GetAllAsync();
     }
+
+    public async Task<Vehicle?> GetAsync(Guid id)
+    {
+        VehicleRepository vehicleRepository = new VehicleRepository();
+
+        return await vehicleRepository.GetAsync(id);
+    }
 }
