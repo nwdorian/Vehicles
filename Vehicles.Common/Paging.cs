a0c3d1f39d3c661c7ce5bd6cf167db4f1c929ded
nwdorian <neuwirthdorian@gmail.com>
2024-06-05 17:52:30 +0200

    refactor: add constructors to VehicleFilter, Paging and Sorting classes

--- Vehicles.Common/Paging.cs
+++ Vehicles.Common/Paging.cs
@@ -3,4 +3,10 @@ public class Paging
 {
     public int PageSize { get; set; }
     public int PageNumber { get; set; }
+
+    public Paging(int pageSize, int pageNumber)
+    {
+        PageSize = pageSize;
+        PageNumber = pageNumber;
+    }
 }
--- Vehicles.Common/Sorting.cs
+++ Vehicles.Common/Sorting.cs
@@ -3,4 +3,9 @@ public class Sorting
 {
     public string? OrderBy { get; set; }
     public string? SortOrder { get; set; }
+    public Sorting(string orderBy, string sortOrder)
+    {
+        OrderBy = orderBy;
+        SortOrder = sortOrder;
+    }
 }
