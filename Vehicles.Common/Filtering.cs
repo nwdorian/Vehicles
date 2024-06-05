3df9d862f04fa6115919687d118796754dfaa850
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-06-05 11:09:28 +0200

    feat: add Filtering class

---
+++ Vehicles.Common/Filtering.cs
@@ -0,0 +1,11 @@
+﻿namespace Vehicles.Common;
+public class Filtering
+{
+    public Guid MakeId { get; set; }
+    public string? Model { get; set; }
+    public string? Color { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public bool ForSale { get; set; }
+    public string? SearchQuery { get; set; }
+}
