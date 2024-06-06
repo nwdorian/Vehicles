7df86d6a261c1d290e475d67ce2b73d0f3e37b3e
nwdorian <neuwirthdorian@gmail.com>
2024-06-06 19:27:19 +0200

    feat: add AssemblyScanModule

---
+++ Vehicles.Common/Autofac/AssemblyScanModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace Vehicles.Common.Autofac;
+public abstract class AssemblyScanModule : Module
+{
+    protected abstract Assembly Assembly { get; }
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterAssemblyTypes(Assembly)
+            .AsImplementedInterfaces();
+    }
+
+}
